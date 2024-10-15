8e0047a11e5820aa4427e57169d936202db1e319
Ivania Zambrano <132124499+ZambranoIvania3A@users.noreply.github.com>
2024-10-15 18:00:15 -0500

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'password' => self::$password ?: self::$password = bcrypt('password'), // Contraseña estática para pruebas
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
